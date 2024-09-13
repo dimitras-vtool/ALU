61e2de21014bc476ce2851c0da0e9970ff57ba6a
dimiitras <dimitras@thevtool.com>
2024-09-13 14:10:43 +0300

    revert 3 Revert "corrected error in cs_registers -> data weren't saved correctly in FIFO_IN. Some changes in the way slv_err is produced -> combinationally now"
    This reverts commit 68db2fa7769cbe21323f39de7c84060efbf6a2dc.

--- Common/posedge_detector.v
+++ Common/posedge_detector.v
@@ -14,23 +14,13 @@ output positive_edge;
 
 wire sig_dly;
 
-
 d_ff_async_en #(.SIZE(1'b1),
-             .RESET_VALUE(1))
+             .RESET_VALUE(0))
     posedge_reg(.clk(clk),
               .rst(!rst_n),
 			  .en(1'b1),
               .d(sig_to_detect),
               .q(sig_dly));
-
-/*
-always@(posedge clk, negedge rst_n) begin
-    if(!rst_n) 
-        sig_dly <= RESET_VALUE;
-    else if(en)
-        sig_dly <= sig_to_detect;
-end
-*/
 
 assign positive_edge = (!sig_dly & sig_to_detect);
 
